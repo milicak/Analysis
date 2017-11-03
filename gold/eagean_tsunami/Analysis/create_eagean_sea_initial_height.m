4c6baa1e206923440dee9fde49340410cf906d2b
milicak <ilicakme@gmail.com>
2017-11-03 23:26:50 +0100

    new updates

--- gold/eagean_tsunami/Analysis/create_eagean_sea_initial_height.m
+++ gold/eagean_tsunami/Analysis/create_eagean_sea_initial_height.m
@@ -1,15 +1,21 @@
 clear all
 
 northdip = 1;
+ShapeName = 'northdip'
 
-if northdip 
+switch ShapeName
+    case char('northdip')
     fname = 'static_syn_northdip'
     outname = 'Eagean_Sea_mosaicN2275M1625_northdip.nc';
     %outname = 'Eagean_Sea_mosaicN1400M1500_northdip.nc';
-else
+    case char('southdip')
     fname = 'static_syn_southdip'
     outname = 'Eagean_Sea_mosaicN2275M1625_southdip.nc';
     %outname = 'Eagean_Sea_mosaicN1400M1500_southdip.nc';
+    case char('gaussdist')
+    fname = 'gaussian_initial.mat';
+    outname = 'Eagean_Sea_mosaicN2275M1625_gaussian.nc';
+end
 end
 
 %grdname = 'Eagean_Sea_mosaicN2275M1625.nc';
@@ -29,12 +35,25 @@ lon = ll1(2:2:end,2:2:end);
 lat = ll2(2:2:end,2:2:end);
 
 a = load(fname);
-
-lon1 = reshape(a(:,1),[51 89]);
-lat1 = reshape(a(:,2),[51 89]);
-zeta = reshape(a(:,3),[51 89]);
-%convert cm to meter;
-zeta = zeta.*1e-2;
+switch ShapeName
+    case char('northdip')
+    lon1 = reshape(a(:,1),[51 89]);
+    lat1 = reshape(a(:,2),[51 89]);
+    zeta = reshape(a(:,3),[51 89]);
+    %convert cm to meter;
+    zeta = zeta.*1e-2;
+    case char('southdip')
+    lon1 = reshape(a(:,1),[51 89]);
+    lat1 = reshape(a(:,2),[51 89]);
+    zeta = reshape(a(:,3),[51 89]);
+    %convert cm to meter;
+    zeta = zeta.*1e-2;
+    case char('gaussdist')
+    lon1 = a.lon; 
+    lat1 = a.lat;
+    zeta = a.ssh;
+ end
+end
 
 
 %initial wave is zero
