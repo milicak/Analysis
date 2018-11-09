cb2b59b0b2f6fc4c4056726e667ab99a865d33e8
milicak <ilicakme@gmail.com>
2018-11-09 17:50:57 +0300

    Arctic case update

---
+++ mitgcm/Arctic_4km/Analysis/compute_JRA55_forcing_river.m
@@ -0,0 +1,30 @@
+clear all
+close all
+
+root_folder = '/shared/projects/uniklima/globclim/milicak/JRA55/';
+
+fyear = 1958;
+lyear = 2015;
+fyear = 2016;
+lyear = 2016;
+fyear = 2017;
+lyear = 2017;
+
+
+foutnames = {'JRA55_runoff'};
+vars = {'runoff_all'};
+varnames = {'friver'};
+
+for i = 1:length(vars)
+    for year=fyear:lyear
+        year
+        % fname = [root_folder vars{i} '.' num2str(year) '.15Dec2016.nc'];
+        % fname = [root_folder vars{i} '.' num2str(year) '.30Jun2017.nc'];
+        fname = [root_folder vars{i} '.' num2str(year) '.18Mar2018.nc'];
+        tmp = ncread(fname,varnames{i});
+        fout = [root_folder foutnames{i} '_' num2str(year)];
+        writebin(fout,tmp,1,'real*4')
+        clear tmp
+    end % year
+end
+
