7851ada1ce8f926b55239ac76230fa611e10eb62
Mehmet Ilicak <ilicakme@gmail.com>
2019-06-19 10:43:34 +0300

    WRF RAIN added

---
+++ mitgcm/blacksea/Analysis/make_forcing_WRF_RAINNC.m
@@ -0,0 +1,155 @@
+clear all
+rho0 = 1e3;
+mm2m = 1e-3; %mm to meter
+% variable names
+fname = '/media/milicak/DATA1/datasets/WRF/BSA03/RAIN_full.nc'
+varname = 'RAINNC'
+
+freqdata = 6; % six hours data 
+dataperday = 24/freqdata; 
+
+ocngrdname = '/media/milicak/DATA1/datasets/WRF/BSA03/BlackSea_grid_NX_896_NY_440.nc';
+lonmit = ncread(ocngrdname,'nav_lon');
+latmit = ncread(ocngrdname,'nav_lat');
+
+grdname = '/media/milicak/DATA1/datasets/WRF/BSA03/wrf_model_mask.nc';
+lon = ncread(grdname,'XLONG');
+lat = ncread(grdname,'XLAT');
+mask = ncread(grdname,'LANDMASK');
+lon = double(lon);
+lat = double(lat);
+mask = double(mask);
+
+
+index = 0
+year = 2011;
+for itr= 1:365*dataperday %2011
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+index = 0
+year = 2012;
+for itr = 365*dataperday+1 : (365+366)*dataperday %2012
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+index = 0
+year = 2013;
+for itr = (365+366)*dataperday+1 : (365+366+365)*dataperday %2013
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+index = 0
+year = 2014;
+for itr = (365+366+365)*dataperday+1 : (365+366+365+365)*dataperday %2014
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+index = 0
+year = 2015;
+for itr = (365+366+365+365)*dataperday+1 : (365+366+365+365+365)*dataperday %2015
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+index = 0
+year = 2016;
+for itr = (365+366+365+365+365)*dataperday+1 : (365+366+365+365+365+366)*dataperday %2016
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+return
+
+index = 0
+year = 2017;
+for itr = (365+366+365+365+365+366)*dataperday+1 : (365+366+365+365+365+366+365)*dataperday %2017
+    index = index+1;
+    var1 = double(ncread(fname,varname,[1 1 itr],[Inf Inf 1]));
+    var2 = double(ncread(fname,varname,[1 1 itr+1],[Inf Inf 1]));
+    var = rho0*mm2m*(var2-var1)/(freqdata*3600); 
+    dnm = griddata(lon(mask==0),lat(mask==0),var(mask==0), ...
+            lon(mask==1),lat(mask==1),'nearest');
+    var(mask==1) = dnm;
+    varmit(:,:,index) = griddata(lon,lat,var,lonmit,latmit);
+end
+oname = [varname '_' num2str(year) '.data'];
+fid=fopen(oname,'w','b');                             
+fwrite(fid,varmit,'real*4');                             
+fclose(fid)                                           
+clear varmit
+
+
+
+
+
